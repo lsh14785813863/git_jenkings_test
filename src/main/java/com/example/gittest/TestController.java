973d548148d579770f029d15e35adad7ad8bcb46
李树洪 <lishuhong@qiyuezn.com>
2020-08-17 18:43:34 +0800

    test11

--- src/main/java/com/example/gittest/TestController.java
+++ src/main/java/com/example/gittest/TestController.java
@@ -3,11 +3,6 @@ package com.example.gittest;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-/**
- * @Author 李树洪
- * @Email 2324592742@qq.com
- * @Date 2020/8/17 18:10
- */
 @RestController
 public class TestController {
     @RequestMapping("test")
